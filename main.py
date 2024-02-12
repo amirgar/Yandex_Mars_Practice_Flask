15ab6aa26c53f93ebceeb071b7e2bcde30ed1cb4
amirgar <garam08@bk.ru>
2024-02-12 19:09:20 +0300

    Добавлены изменения для выполнения задания "Результат отбора".

--- main.py
+++ main.py
@@ -114,5 +114,32 @@ def plan_name(planet_name):
                     </html>"""
 
 
+@app.route('/results/<nickname>/<int:level>/<float:rating>')
+def results(nickname, level, rating):
+    return f"""<!doctype html>
+                    <html lang="ru">
+                      <head>
+                        <meta charset="utf-8">
+                        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+                        <link rel="stylesheet" 
+                        href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" 
+                        integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" 
+                        crossorigin="anonymous">
+                        <title>Привет, Марс!</title>
+                      </head>
+                      <body>
+                        <h1>Результаты отбора</h1>
+                        <h3>Претендент на участие в миссии, {nickname}:</h3>
+                        <div class="alert alert-primary" role="alert">
+                            <br> Поздравляем, Ваш рейтинг после {level} этапа отбора:
+                        </div>
+                        <h3>составляет {rating}!</h3>
+                        <div class="alert alert-primary" role="alert">
+                        <br> Желаем удачи!
+                        </div>
+                      </body>
+                    </html>"""
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
